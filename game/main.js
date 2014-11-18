3be177cc00426a5d24a7b802fb4c223c25d3173a
lnxslackware <lnxslackware@gmail.com>
2014-11-18 23:46:42 +0200

    Revert "Merge branch 'master' of https://github.com/BarishYumerov/Team---Caspia-"
    This reverts commit 23a0e7dc77865311b51b008d545c8fbf2fc90cbf, reversing
    changes made to 05ec833ad7092807ac181aaef07629eb36576e67.

--- game/main.js
+++ game/main.js
@@ -3,7 +3,6 @@ var peshoPic = new Image();
 peshoPic.src='images/pesho-1.png';
 var barrelPic = new Image();
 barrelPic.src = 'images/barrel.png';
-<<<<<<< HEAD
 var x = 1000;
 
 function pesho() {
@@ -11,40 +10,11 @@ function pesho() {
 }
 
 function barrel() {
-=======
-var rocks = [];
-
-var yPesho=90;
-var xBarrel = 300;
-var fast = 40;
-var rnd = [];
-for( var i = 0 ; i < 1000; i++){
-    rnd[i] = Math.floor(Math.random()*(80-16+1)+16);
-}
-
-function pesho() {
-    ctx.drawImage(peshoPic, 20, yPesho, 35, 35);
-}
-function jump(){
->>>>>>> 46cbe583e0659e53dcdcf51e3977c645b8d51a0a
     ctx.save();
     ctx.drawImage(barrelPic, x, 360, 50, 50);
     ctx.restore();
-<<<<<<< HEAD
     x -= 2;
 }
-=======
-
-}
-function barrel(){
-    ctx.save();
-    for( var i = 0; i < 1000 ; i++){
-        rocks[i] = ctx.drawImage(barrelPic, xBarrel + i*rnd[i], 110, 15, 15);
-    }
-    ctx.restore();
-    xBarrel -= 2;
-}
->>>>>>> 46cbe583e0659e53dcdcf51e3977c645b8d51a0a
 
 function background() {
     grd = ctx.createLinearGradient(0,0,0, cHeight);
@@ -71,11 +41,7 @@ function init() {
     pesho();
     barrel();
     ctx.restore();
-    var loopTimer = setTimeout('init('+')',fast);
-    if(fast > 10) {
-        fast -= 0.02;
-    }
-
+    var loopTimer = setTimeout('init('+')',10);
 }
 
 window.addEventListener('load', init);
